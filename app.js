ee987397cf58c6a8c5b3bdb27d98a3bdf1a491a2
GLADDetailing <124705867+GLADDetailing@users.noreply.github.com>
2023-03-30 14:28:35 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+let popup = document.getElementById("popup1");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
